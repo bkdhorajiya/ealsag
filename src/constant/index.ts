ffbb6e9d3f16ed58b6ff9b95523a373c39c0505e
bkdhorajiya <112803763+bkdhorajiya@users.noreply.github.com>
2025-06-10 13:18:13 +0530

    Update contract address

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -2,7 +2,7 @@
 import { ethers } from 'ethers';
 import { ELECTION_CONFIG } from "@/config/election";
  
-export const VOTING_CONTRACT_ADDRESS = "0x4cc65EC8EEfaBaBB7A6123BC326F48FdD15ccC44";
+export const VOTING_CONTRACT_ADDRESS = "0x8b769c68d9B7c734BB5afA5412C0B18D9FeFEE0B";
 export const VOTING_CONTRACT_ABI = [
   {
     "inputs": [
